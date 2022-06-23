8cc7c5097bcca4b48b3139796af64852964509ed
PHYLLIS OSE <105457730+Osephyllis@users.noreply.github.com>
2022-06-23 20:47:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let name = "AIMANKHU PHYLLIS";
+console.log(name);
+document.getElementById("demo").innerHTML = "AIMANKHU PHYLLIS";
+
+let height = "5foot 7inches";
+console.log(height);
+document.getElementById("demo1").innerHTML = "5foot 7inches";
+
+let country = "Nigeria";
+console.log(country);
+document.getElementById("demo2").innerHTML = "Nigeria";
+
